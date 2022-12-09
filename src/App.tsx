f77b7bc86e5d2a7e4324c7f45c69d5b9a18891c3
kolisovet <kolisovet@gmail.com>
2022-12-09 10:23:13 +0700

    tasks 2

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,15 @@ function App() {
         {id: 2, title: "JS6 & TS", isDone: true},
         {id: 3, title: "REACT", isDone: false},
     ]
+    const tasks_2: Array<TaskType> = [
+        {id: 1, title: "Bread", isDone: true},
+        {id: 2, title: "Cheese", isDone: true},
+        {id: 3, title: "Beer", isDone: false},
+    ]
     return (
         <div className="App">
             <TodoList title={todoListTitle_1} tasks={tasks_1}/>
-            {/*<TodoList title={todoListTitle_2} tasks={""}/>*/}
+            <TodoList title={todoListTitle_2} tasks={tasks_2}/>
 
         </div>
     );
